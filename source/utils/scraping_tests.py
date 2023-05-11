8b1313b6702c0397502b5f8d6b369a9c716785c9
Dastan312 <dastanmirzayev88@gmail.com>
2023-05-11 11:01:36 +0200

    added last one

---
+++ source/utils/scraping_tests.py
@@ -0,0 +1,27 @@
+
+
+import requests
+import json
+
+from bs4 import BeautifulSoup
+
+
+immo_url = "https://www.immoweb.be/fr/annonce/immeuble-mixte/a-vendre/alost/9300/10557403"
+
+
+cookie_url="https://www.immoweb.be/cookie"
+
+user_cookie = requests.get(cookie_url).cookies
+
+
+response = requests.get(immo_url, cookies = user_cookie, params = {})
+ 
+with open('single_building_info.json', 'w') as f:
+    json.dump(response.text, f)
+
+
+
+
+
+
+
