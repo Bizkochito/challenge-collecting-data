cf46b5421cb1f640d485363dabfb2b198d54c413
Gregoire Hupin <hupingregoire@hotmail.com>
2023-05-12 09:14:08 +0200

    Some changes to urls_scraper.

--- source/utils/scraping_tests.py
+++ source/utils/scraping_tests.py
@@ -2,7 +2,7 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-immo_url = "https://www.immoweb.be/fr/annonce/immeuble-mixte/a-vendre/alost/9300/10557403"
+""" immo_url = "https://www.immoweb.be/fr/annonce/immeuble-mixte/a-vendre/alost/9300/10557403"
 cookie_url="https://www.immoweb.be/cookie"
 
 user_cookie = requests.get(cookie_url).cookies
@@ -12,4 +12,9 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 with open('single_building_info.json', 'w') as f:
     json.dump(response.json(), f)
-
+ """
+immo_base_url = "https://www.immoweb.be/fr/recherche/maison/a-vendre?countries=BE&page="
+immo_end_url = "&orderBy=relevance"
+for index in range(1,333):
+    full_url = immo_base_url + str(index) + immo_end_url
+    print(full_url)
