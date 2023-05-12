851755f1baa44cc96b27be325bad1cb727075ac6
Gregoire Hupin <hupingregoire@hotmail.com>
2023-05-12 09:41:04 +0200

    Refactored urls_scraper.py into a function returning 30 urls

--- source/utils/urls_scraper.py
+++ source/utils/urls_scraper.py
@@ -1,27 +1,26 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+from typing import List
 
-immo_url = "https://www.immoweb.be/fr/recherche/maison/a-vendre?countries=BE&page=1&orderBy=relevance"
-cookie_url="https://www.immoweb.be/cookie"
 
+"""
+This code can be used later on to loop through pages. Generates URLS with page number 1->333
+immo_base_url = "https://www.immoweb.be/fr/recherche/maison/a-vendre?countries=BE&page=""
+immo_end_url = "&orderBy=relevance"
+for index in range(1,333):
+    full_url = immo_base_url + str(index) + immo_end_url
+    print(full_url)
 user_cookie = requests.get(cookie_url).cookies
+"""
+def extract_urls_lists(search_page_url: str) -> List(str):
+    response = requests.get(search_page_url, params = {})
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-response = requests.get(immo_url, cookies = user_cookie, params = {})
-soup = BeautifulSoup(response.text, 'html.parser')
+    list_of_urls=[]
 
-stuff=[]
-# retrieve all of the paragraph tags
-for article in soup.find_all('iw-search-card-rendered'):
-    for link in article.find_all('a'):
-        print(link.get('href'))
-        stuff.append(link)
-
-#with open('prettysoup.txt', 'w') as f:
-#    f.write(soup.prettify())
-with open('deepersoup.txt', 'w') as f:
-    f.write(str(stuff))
-
-#with open('immoweb_urls.json', 'w') as f:
-#   json.dump(stuff, f)
+    for article in soup.find_all('iw-search-card-rendered'):
+        for link in article.find_all('a'):
+            list_of_urls.append(link.get('href'))
+    return list_of_urls
 
